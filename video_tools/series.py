33e81ff4b328b6f5e27e6e9a5328aa2370027109
Anders Birkenes <datagutten@datagutten.net>
2026-03-07 14:07:24 +0100

    Find episodes with lower case

--- video_tools/series.py
+++ video_tools/series.py
@@ -11,15 +11,15 @@ def find_episodes(path: Path, extensions=None) -> dict[str, Path]:
         if episode.is_dir():
             episodes.update(find_episodes(episode, extensions))
         else:
-            matches = re.search(r'(S\d+E\d+)', episode.name)
+            matches = re.search(r'(S\d+E\d+)', episode.name, re.IGNORECASE)
             if not matches:
                 continue
             if extensions is not None and episode.suffix not in extensions:
                 continue
-            episodes[matches.group(1)] = episode
+            episodes[matches.group(1).upper()] = episode
     return episodes
 
 
 def parse_episode(episode_str: str):
-    matches = re.search(r'S(\d+)E(\d+)', episode_str)
+    matches = re.search(r'S(\d+).?EP?(\d+)', episode_str, re.IGNORECASE)
     return int(matches.group(1)), int(matches.group(2))
